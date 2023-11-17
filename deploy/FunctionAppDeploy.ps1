9c243f15da41558bbc20cf42d88960f8ce2a69ec
brett.wolf.howells <brett.wolf.howells@gmail.com>
2023-11-17 14:05:31 -0500

    Adding deploy powershell scripts

---
+++ deploy/FunctionAppDeploy.ps1
@@ -0,0 +1,15 @@
+﻿
+
+# Create a zip file in the root of the deploy folder with the function app code
+$FunctionAppFolder = -join((get-item $PSScriptRoot ).parent.FullName, "\fortune_handler_function")
+$ZipDestination = -join($PSScriptRoot, '\Function.zip')
+$FilesToExclude = @(".funcignore",".gitignore")
+$FunctionFilesToZip = Get-ChildItem -Path $FunctionAppFolder -Exclude $FilesToExclude
+Compress-Archive -Path $FunctionFilesToZip -DestinationPath $ZipDestination -CompressionLevel Fastest
+
+Connect to azure account and deploy zipped function code
+Connect-AzAccount
+Publish-AzWebapp -ResourceGroupName rg-fortune-handler -Name func-fortune-handler -ArchivePath $ZipDestination
+
+
+
---
+++ deploy/WebAppDeploy.ps1
@@ -0,0 +1,10 @@
+﻿# Create a zip file in the root of the deploy folder with the html app code
+$WebAppFolder = -join((get-item $PSScriptRoot ).parent.FullName, "\fortune_website")
+
+$ZipDestination = -join($PSScriptRoot, '\WebApp.zip')
+$WebAppFilesToZip = Get-ChildItem -Path $WebAppFolder
+
+Write-Output $WebAppFilesToZip
+Compress-Archive -Path $WebAppFolder -DestinationPath $ZipDestination -CompressionLevel Fastest
+
+Publish-AzWebapp -ResourceGroupName rg-fortune-handler -Name web-fortune-handler -ArchivePath $ZipDestination
